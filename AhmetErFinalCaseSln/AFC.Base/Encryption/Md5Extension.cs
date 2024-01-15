b2c572a2a090e83d5dd0626f91f81aab26cfa0ed
Ahmet Er <ahmet.er.14@gmail.com>
2024-01-15 09:06:04 +0300

    Md5Extension, ApiResponse, JwtConfig

---
+++ AhmetErFinalCaseSln/AFC.Base/Encryption/Md5Extension.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AFC.Base.Encryption;
+
+public class Md5Extension
+{
+    public static string Create(string input)
+    {
+        using (MD5 md5 = MD5.Create())
+        {
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            return Convert.ToHexString(hashBytes).ToLower();
+        }
+    }
+    public static string GetHash(string input)
+    {
+        var hash = Create(input);
+        return Create(hash);
+    }
+}
